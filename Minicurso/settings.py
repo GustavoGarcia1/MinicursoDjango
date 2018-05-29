ec906a10d5a1bd0585b005ebba8f50a79f35e55f
diego <diegodiasptc@gmail.com>
2018-05-29 08:19:25 -0300

    Adicionando Models e mudando Settings para PT-BR

--- Minicurso/settings.py
+++ Minicurso/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Minicurso.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'testeDB'),
     }
 }
 
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- conta/models.py
+++ conta/models.py
@@ -1,2 +1,31 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+class Conta(models.Model):
+    ag = models.IntegerField()
+    conta = models.IntegerField()
+    digito = models.IntegerField(MinValueValidator(MinValueValidator(0), MaxValueValidator(9)))
+    saldo = models.DecimalField(max_digits=7, decimal_places=2)
+
+class Titulo(models.Model):
+    conta = models.ForeignKey(Conta, on_delete=models.SET_NULL, null=True)
+    descricao = models.CharField(max_length=50)
+    data = models.DateField()
+    observacao = models.TextField(null=True, blank=True)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+class Titulo_Pagar(Titulo):
+    data_vencimento = models.DateField()
+    quitado = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Titulos A Pagar"
+
+class Titulo_Receber(Titulo):
+    data_receber = models.DateField()
+    recebido = models.BooleanField(default=False)
+
+
+    class Meta:
+        verbose_name_plural = "Titulos A Receber"
